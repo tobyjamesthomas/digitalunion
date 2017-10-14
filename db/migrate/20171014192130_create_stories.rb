c63d8740674718678f6d052f87e03e57ba68f20a
Toby <toby.james.thomas@gmail.com>
2017-10-14 15:34:58 -0400

    add stories and sign in links

---
+++ db/migrate/20171014192130_create_stories.rb
@@ -0,0 +1,11 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      t.string :title
+      t.integer :score, :default => 0
+      t.string :body
+
+      t.timestamps null: false
+    end
+  end
+end
